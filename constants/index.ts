ce56af3cb54564d1057571d3c8aedf48c51391d3
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-03 21:57:26 +0500

    Exported constants for Hover component

---
+++ constants/index.ts
@@ -0,0 +1,24 @@
+import { c2s, locomotive, officestudio, silencio } from "@public";
+
+export const project = [
+    {
+        title: "C2 Montreal",
+        img: c2s,
+        color: "#000000",
+    },
+    {
+        title: "Office Studio",
+        img: officestudio,
+        color: "#8C8C8C",
+    },
+    {
+        title: "Locomotive",
+        img: locomotive,
+        color: "#EFE8D3",
+    },
+    {
+        title: "Silencio",
+        img: silencio,
+        color: "#706D63",
+    },
+];
