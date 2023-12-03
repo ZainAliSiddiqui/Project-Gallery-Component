5d6ca36603152531842ce8ed46603b60204ed6aa
Zain Ali <ZainSoftwear11@gmail.com>
2023-12-03 21:58:56 +0500

    Exported images for Hover component

---
+++ public/index.ts
@@ -0,0 +1,4 @@
+export { default as c2s } from './c2montreal.png';
+export { default as locomotive } from './locomotive.png';
+export { default as silencio } from './silencio.png';
+export { default as officestudio } from './officestudio.png';
